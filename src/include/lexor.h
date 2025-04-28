43e86a5dd3b07208ab65511620eff0b477bce98f
Dan <56096498+halleydt@users.noreply.github.com>
2025-04-28 17:51:58 -0400

    started work on lexor

---
+++ src/include/lexor.h
@@ -0,0 +1,15 @@
+#ifndef LEXER_H
+#define LEXER_H
+typedef struct LEXER_STRUCT
+{
+	char C;
+	unsigned int i;
+	char* contents;
+} lexer_T;
+
+leser_T* init_lexer(char* contents);
+
+void lexer_advance(lexer_T* lexer);
+
+void lexer_skip_whitespace(lexer_T* lexer);
+#endif
