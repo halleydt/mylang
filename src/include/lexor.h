ad21b6d74b5fde8e63bf353b960012af1f9fed68
Dan <56096498+halleydt@users.noreply.github.com>
2025-04-30 17:54:20 -0400

    started lexer.c
    fixed error in include/lexor.c

--- src/include/lexor.h
+++ src/include/lexor.h
@@ -1,5 +1,7 @@
 #ifndef LEXER_H
 #define LEXER_H
+#include "token.h"
+
 typedef struct LEXER_STRUCT
 {
 	char C;
@@ -7,9 +9,15 @@ typedef struct LEXER_STRUCT
 	char* contents;
 } lexer_T;
 
-leser_T* init_lexer(char* contents);
+lexer_T* init_lexer(char* contents);
 
 void lexer_advance(lexer_T* lexer);
 
 void lexer_skip_whitespace(lexer_T* lexer);
+
+token_T* lexer_get_next_token(lexer_T* lexer);
+
+token_T* lexer_collect_string(lexer_T* lexer);
+
+char* lexer_get_current_char_as_string(lexer_T* lexer);
 #endif
